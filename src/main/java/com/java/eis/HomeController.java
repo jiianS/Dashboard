3efc3895921d576aa35a2b9e5bfd54983111f70d
goodee <goodee@DESKTOP-2LBSE6V>
2018-07-31 11:49:20 +0900

    chardata 생성 (data - > 고정값)

--- src/main/java/com/java/eis/HomeController.java
+++ src/main/java/com/java/eis/HomeController.java
@@ -1,8 +1,10 @@
 package com.java.eis;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import javax.annotation.Resource;
@@ -44,8 +46,35 @@ public class HomeController {
 	@RequestMapping("getBar")
 	public void getBar(HttpServletResponse resp) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
-		result.put("result",session.selectList("eis.selectBar"));
 		
+		/*********************************************************************************/
+		List<HashMap<String, Object>> resultList = session.selectList("eis.selectBar");
+		List<HashMap<String, Object>> columns = new ArrayList<HashMap<String,Object>>();
+		
+		HashMap<String, Object> columnMap = new HashMap<String, Object>();
+		columnMap.put("type", "string");
+		columnMap.put("value", "city");
+		columns.add(columnMap);
+		
+		columnMap = new HashMap<String, Object>();
+		columnMap.put("type", "number");
+		columnMap.put("value", "Population2000");
+		columns.add(columnMap);
+		
+		columnMap = new HashMap<String, Object>();
+		columnMap.put("type", "number");
+		columnMap.put("value", "Population2000");
+		columns.add(columnMap);
+		
+//		같은 주소에 넣게 하지 않기 위해, columnMap을 새로 생성해서 각각 넣어준다. 
+//		columns.add("city");
+//		columns.add("Population2000");
+//		columns.add("Population2000");
+		
+		result.put("result",resultList);
+		result.put("size",resultList.size());
+		result.put("columns", columns);
+		/*********************************************************************************/
 		//result를 보낼떄, utf8로 인코딩하고, json타입으로 보내려고 json타입으로 변환시켜주기
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/json;charset=utf-8");
@@ -63,3 +92,4 @@ public class HomeController {
 	}
 
 }
+
